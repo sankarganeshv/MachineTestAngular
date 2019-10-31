1014f51e00728991a045741be89284f1f5b790d2
unknown <sankarganesh2709>
2019-10-31 19:12:08 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { MainServiceService } from '../main-service.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private _service:MainServiceService, private router:Router) { }
+   b:boolean;
+   message:string;
+
+  ngOnInit() {
+  }
+
+
+  loginUser(event)
+  {
+event.preventDefault();
+console.log(event);
+
+var username=event.target.elements[0].value;
+var password=event.target.elements[1].value;
+
+this._service.verifyUser(username,password)
+.subscribe((userData) =>{
+  this.b=userData;
+  console.log(this.b);
+
+  if(this.b==true)
+  {
+    this.router.navigate(['view']);
+  }
+  else{
+    this.router.navigate(['login'])
+    this.message="sorry username or password is invalid"
+
+  }
+
+}, (error) =>{
+  console.log(error);
+  
+});
+ }
+
+
+}
